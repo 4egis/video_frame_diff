b1f4e40d22af52a568065c39bd8ac6085c11101b
4egis <jedrek.rymsza@gmail.com>
2023-08-31 19:25:04 +0200

    Refactor and Optimize Video Analysis Script
    Refactored code for enhanced readability, maintainability, and performance.
    Major changes include:
    
    - Centralized DOM element selection to reduce redundancy.
    - Separated functionalities into distinct, well-named functions with detailed comments.
    - Replaced anonymous functions in event listeners with named functions for better readability and debugging.
    - Optimized canvas usage by creating it only once and setting dimensions during the loadedmetadata event.
    Introduced fps variable for frame analysis speed optimization.
    - Modularized key functionalities like pixel intensity calculation and CSV exporting.
    - Utilized template literals for string manipulation and added performance logging.

--- script.js
+++ script.js
@@ -1,88 +1,159 @@
-let video = document.getElementById('videoPlayer');
-let csvData = [];
+// DOM element selectors.
+const videoPlayer = document.getElementById('videoPlayer');
+const videoInput = document.getElementById('videoInput');
+const splitLine = document.getElementById('splitLine');
+const splitLineVisual = document.getElementById('splitLineVisual');
 
-document.getElementById('videoInput').addEventListener('change', function() {
+// Global variables.
+let csvData = [];
+let canvas = createCanvas();
+let ctx = canvas.getContext('2d');
+let lastImageData = null;
+
+// Event listeners.
+videoInput.addEventListener('change', handleVideoInput);
+videoPlayer.addEventListener('loadedmetadata', handleVideoMetadata);
+window.addEventListener('resize', updateLinePosition);
+
+/**
+ * Handles changes to the video input.
+ */
+function handleVideoInput() {
   const file = this.files[0];
   const url = URL.createObjectURL(file);
-  video.src = url;
-});
+  videoPlayer.src = url;
+}
 
+/**
+ * Handles video metadata loading.
+ */
+function handleVideoMetadata() {
+  updateLinePosition();
+  canvas.width = videoPlayer.videoWidth;
+  canvas.height = videoPlayer.videoHeight;
+}
+
+/**
+ * Creates a new canvas element.
+ * @return {HTMLCanvasElement} The newly created canvas element.
+ */
+function createCanvas() {
+  return document.createElement('canvas');
+}
+
+/**
+ * Updates the position of the split line.
+ */
 function updateLinePosition() {
-  const splitLinePosition = document.getElementById('splitLine').value;
-  const videoWidth = document.getElementById('videoPlayer').offsetWidth;
+  const splitLinePosition = splitLine.value;
+  const videoWidth = videoPlayer.offsetWidth;
   const linePosition = (splitLinePosition / 100) * videoWidth;
-  document.getElementById('splitLineVisual').style.left = linePosition + 'px';
+  splitLineVisual.style.left = `${linePosition}px`;
 }
 
-document.getElementById('videoPlayer').addEventListener('loadedmetadata', function() {
-  updateLinePosition();
-});
-
-window.addEventListener('resize', function() {
-  updateLinePosition();
-});
-
-// Analyze frames by counting average pixel intensity difference between each two consecutive frames
-// and saves it in global variable 'csvData'
+/**
+ * Analyzes frames from the video.
+ */
 function analyzeFrames() {
-  csvData = [["Frame number", "Left Average Pixel Intensity Difference", "Right Average Pixel Intensity Difference"]];
-  let canvas = document.createElement('canvas');
-  let ctx = canvas.getContext('2d');
-  let lastImageData = null;
-  let splitLinePos = document.getElementById("splitLine").value / 100;
+  console.time('Execution Time');
+  const fps = 5;
+  csvData = [
+    ['Frame number', 'Left Average Pixel Intensity Difference', 'Right Average Pixel Intensity Difference']
+  ];
+  const splitLinePos = splitLine.value / 100;
+  const splitLinePixelPos = Math.floor(canvas.width * splitLinePos);
 
-  video.addEventListener('seeked', function() {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-
+  /**
+   * On video seeked, updates the frame and does the analysis.
+   */
+  function onSeeked() {
+    updateFrame(canvas, ctx, videoPlayer);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
     if (lastImageData) {
-      let leftDiff = 0, rightDiff = 0;
-      let splitLinePixelPos = Math.floor(canvas.width * splitLinePos);
-
-      for (let i = 0; i < imageData.length; i+=4) {
-        let x = (i / 4) % canvas.width;
-        let diff = Math.abs(imageData[i] - lastImageData[i]);
-
-        if (x < splitLinePixelPos) {
-          leftDiff += diff;
-        } else {
-          rightDiff += diff;
-        }
-      }
-      
-      let leftAvgDiff = leftDiff / (canvas.width * canvas.height * splitLinePos);
-      let rightAvgDiff = rightDiff / (canvas.width * canvas.height * (1 - splitLinePos));
-
-      csvData.push([video.currentTime, leftAvgDiff, rightAvgDiff]);
+      const diffs = calculatePixelIntensityDiff(
+          imageData, lastImageData, splitLinePixelPos, canvas.width, canvas.height);
+      csvData.push([videoPlayer.currentTime, diffs.leftAvgDiff, diffs.rightAvgDiff]);
     }
-    
     lastImageData = new Uint8Array(imageData);
-    
-    if (video.currentTime < video.duration) {
-      video.currentTime += 1;
-    } else {
-      video.removeEventListener('seeked', arguments.callee);
-      alert("Analysis complete!");
-    }
-  });
 
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  video.currentTime = 0;
+    if (videoPlayer.currentTime < videoPlayer.duration) {
+      videoPlayer.currentTime += 1.0 / fps;
+    } else {
+      videoPlayer.removeEventListener('seeked', onSeeked);
+      console.log('Analysis execution time');
+      console.timeEnd('Execution Time');
+      exportCsv();
+    }
+  }
+
+  videoPlayer.addEventListener('seeked', onSeeked);
+  videoPlayer.currentTime = 0;
 }
 
-// Exports data to csv file 
-// Data is stored in following way:
-// [Frame number, Left Average Pixel Intensity Difference, Right Average Pixel Intensity Difference]
-function exportCSV() {
-  let csvContent = csvData.map(e => e.join(",")).join("\n");
-  let blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-  let link = document.createElement("a");
-  let url = URL.createObjectURL(blob);
-  link.setAttribute("href", url);
-  link.setAttribute("download", "motion_analysis.csv");
-  link.style.visibility = 'hidden';
+/**
+ * Draws the current video frame on the canvas.
+ * @param {HTMLCanvasElement} canvas - The canvas element.
+ * @param {CanvasRenderingContext2D} ctx - The canvas context.
+ * @param {HTMLVideoElement} video - The video element.
+ */
+function updateFrame(canvas, ctx, video) {
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+}
+
+/**
+ * Calculates the pixel intensity differences.
+ * @param {Uint8Array} imageData - The image data.
+ * @param {Uint8Array} lastImageData - The last image data.
+ * @param {number} splitLinePixelPos - The split line position in pixels.
+ * @param {number} width - The canvas width.
+ * @param {number} height - The canvas height.
+ * @return {Object} The average pixel intensity differences.
+ */
+function calculatePixelIntensityDiff(imageData, lastImageData, splitLinePixelPos, width, height) {
+  let leftDiff = 0;
+  let rightDiff = 0;
+  const splitLinePos = splitLinePixelPos / width;
+
+  for (let i = 0; i < imageData.length; i += 4) {
+    const x = (i / 4) % width;
+    const diff = Math.abs(imageData[i] - lastImageData[i]);
+
+    if (x < splitLinePixelPos) {
+      leftDiff += diff;
+    } else {
+      rightDiff += diff;
+    }
+  }
+
+  const leftAvgDiff = leftDiff / (width * height * splitLinePos);
+  const rightAvgDiff = rightDiff / (width * height * (1 - splitLinePos));
+
+  return {leftAvgDiff, rightAvgDiff};
+}
+
+/**
+ * Exports the CSV data.
+ */
+function exportCsv() {
+  const csvContent = csvData.map(e => e.join(',')).join('\n');
+  const blob = new Blob([csvContent], {type: 'text/csv;charset=utf-8;'});
+  const link = createDownloadLink(blob, 'motion_analysis.csv');
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+}
+
+/**
+ * Creates a download link for the file.
+ * @param {Blob} blob - The blob data.
+ * @param {string} fileName - The file name.
+ * @return {HTMLAnchorElement} The created download link.
+ */
+function createDownloadLink(blob, fileName) {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.setAttribute('href', url);
+  link.setAttribute('download', fileName);
+  link.style.visibility = 'hidden';
+  return link;
 }
